f2560dc4125ef80d761b06f21928b41dd90b546a
blackwaltzone <dustin.heyden@gmail.com>
2023-05-18 23:35:28 -0500

    Add files via upload

--- manifest.lua
+++ manifest.lua
@@ -4,86 +4,155 @@ manifest =
 	{
 		['Actions.lua'] =
 		{
-			path = "Actions.lua"
+			path = "code/Actions.lua"
 		},
 		['Animation.lua'] =
 		{
-			path = "Animation.lua"
+			path = "code/Animation.lua"
 		},
 		['Character.lua'] =
 		{
-			path = "Character.lua"
+			path = "code/Character.lua"
+		},
+		['Dependencies.lua'] =
+		{
+			path = "code/Dependencies.lua"
 		},
 		['Entity.lua'] =
 		{
-			path = "Entity.lua"
+			path = "code/Entity.lua"
 		},
 		['EntityDefs.lua'] =
 		{
-			path = "EntityDefs.lua"
+			path = "code/EntityDefs.lua"
 		},
 		['ExploreState.lua'] =
 		{
-			path = "ExploreState.lua"
+			path = "code/ExploreState.lua"
 		},
-		['larger_map.lua'] = 
+		['FadeState.lua']=
 		{
-			path = "larger_map.lua"
+			path = "code/FadeState.lua"
+		},
+		['FrontMenuState.lua'] =
+		{
+			path = "code/FrontMenuState.lua"
+		},
+		['Icons.lua'] =
+		{
+			path = "code/Icons.lua"
+		},
+		['InGameMenuState.lua'] =
+		{
+			path = "code/InGameMenuState.lua"
+		},
+		['ItemDB.lua'] =
+		{
+			path = "code/ItemDB.lua"
+		},
+		['ItemMenuState.lua'] =
+		{
+			path = "code/ItemMenuState.lua"
 		},
 		['Map.lua'] =
 		{
-			path = "Map.lua"
+			path = "code/Map.lua"
 		},
 		['main.lua'] =
 		{
-			path = "main.lua"
+			path = "code/main.lua"
+		},
+		['MenuLayout.lua'] =
+		{
+			path = "code/MenuLayout.lua"
 		},
 		['MoveState.lua'] = 
 		{
-			path = "MoveState.lua"
+			path = "code/MoveState.lua"
 		},
 		['NPCStandState.lua'] =
 		{
-			path = "NPCStandState.lua"
+			path = "code/NPCStandState.lua"
+		},
+		['Panel.lua'] =
+		{
+			path = "code/Panel.lua"
 		},
 		['PlanStrollState.lua'] =
 		{
-			path = "PlanStrollState.lua"
+			path = "code/PlanStrollState.lua"
+		},
+		['ProgressBar.lua'] =
+		{
+			path = "code/ProgressBar.lua"
+		},
+		['ScrollBar.lua'] =
+		{
+			path = "code/ScrollBar.lua"
+		},
+		['Selection.lua'] =
+		{
+			path = "code/Selection.lua"
 		},
         ['small_room.lua'] =
    		{
-       		path = "small_room.lua"
+       		path = "code/small_room.lua"
     	},
 		['StateMachine.lua'] =
 		{
-			path = "StateMachine.lua"
+			path = "code/StateMachine.lua"
+		},
+		['StateStack.lua'] =
+		{
+			path = "code/StateStack.lua"
+		},
+		['Textbox.lua'] =
+		{
+			path = "code/Textbox.lua"
 		},
 		['Trigger.lua'] =
 		{
-			path = "Trigger.lua"
+			path = "code/Trigger.lua"
 		},
 		['Tween.lua'] =
 		{
-			path = "Tween.lua"
+			path = "code/Tween.lua"
 		},
 		['Util.lua'] =
 		{
-			path = "Util.lua"
+			path = "code/Util.lua"
 		},
 		['WaitState.lua'] =
 		{
-			path = "WaitState.lua"
+			path = "code/WaitState.lua"
+		},
+		['World.lua'] =
+		{
+			path = "code/World.lua"
 		},
 	},
     textures =
     {
         ['walk_cycle.png'] =
         {
-            path = "walk_cycle.png",
+            path = "art/walk_cycle.png",
         },
         ['rpg_indoor.png'] =
         {
-            path = "rpg_indoor.png",
+            path = "art/rpg_indoor.png",
+        },
+        ['cursor.png'] =
+        {
+            path = "art/cursor.png",
+        },
+        ['gradient_panel.png'] =
+        {
+            path = "art/gradient_panel.png",
+            --scale = "pixelart"	-- want it iterpolated, so pixels blend
+        },
+        ['continue_caret.png'] =
+        {
+            path = "art/continue_caret.png",
         },
     }
 }
